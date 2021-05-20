374f2a85c775a62b6f72045bb5c121f5300fcbe0
ggyaal <ggyaal@gmail.com>
2021-05-21 01:20:42 +0900

    movie fake db

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,49 @@
-export const people = [
+let movies = [
+  {
+    id: 0,
+    name: "Star Wars - The new one",
+    score: 1,
+  },
   {
     id: 1,
-    name: "Kim",
-    age: 18,
-    gender: "male",
+    name: "Avengers - The new one",
+    score: 8,
   },
   {
     id: 2,
-    name: "Lee",
-    age: 21,
-    gender: "male",
+    name: "The Godfather I",
+    score: 99,
   },
   {
     id: 3,
-    name: "Park",
-    age: 17,
-    gender: "female",
-  },
-  {
-    id: 4,
-    name: "Chei",
-    age: 23,
-    gender: "female",
-  },
-  {
-    id: 5,
-    name: "Chang",
-    age: 16,
-    gender: "male",
-  },
-  {
-    id: 6,
-    name: "Won",
-    age: 21,
-    gender: "female",
-  },
-  {
-    id: 7,
-    name: "Chung",
-    age: 26,
-    gender: "female",
+    name: "Logan",
+    score: 2,
   },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const getPeople = people.filter((people) => people.id === id);
-  return getPeople[0];
+  const filteredMovies = movies.filter((movie) => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,13 @@
-import { getById, people } from "./db";
+import { getMovies, getById, addMovie, deleteMovie } from "./db";
 
 const resolvers = {
   Query: {
-    people: () => people,
-    person: (obj, { id }) => getById(id),
+    movies: () => getMovies(),
+    movie: (_, { id }) => getById(id),
+  },
+  Mutation: {
+    addMovie: (obj, { name, score }) => addMovie(name, score),
+    deleteMovie: (obj, { id }) => deleteMovie(id),
   },
 };
 
