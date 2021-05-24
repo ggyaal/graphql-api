10778198e13f64644b5f7ad0eadcc54ad136dec4
ggyaal <ggyaal@gmail.com>
2021-05-24 17:08:55 +0900

    build commend

--- src/graphql/db.js
+++ src/graphql/db.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+
 const BASE_URL = "https://yts-proxy.now.sh/";
 const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
 const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
